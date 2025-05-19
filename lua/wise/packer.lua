45585ecb68aea5a2ba561b146dc0433035491128
Fardeen Panjwani <edmond@montecristo.com>
2025-05-19 21:27:39 +0530

    feat: add wakatime

--- lua/wise/packer.lua
+++ lua/wise/packer.lua
@@ -4,18 +4,19 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
-	use {
-		"ellisonleao/gruvbox.nvim",
-		as = "gruvbox"
-	}
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.8',
-		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
-	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-	use('theprimeagen/harpoon')
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  use {
+    "ellisonleao/gruvbox.nvim",
+    as = "gruvbox"
+  }
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    -- or                            , branch = '0.1.x',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use('theprimeagen/harpoon')
+  use 'wakatime/vim-wakatime'
 end)
 
